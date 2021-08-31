1cdae8e3c3f45f569b0169d69d687a5676d48cc3
Nikolle de Lacerda Nascimento <67565460+nikollelacerda@users.noreply.github.com>
2021-08-30 22:49:25 -0300

    Add files via upload

---
+++ docs/script.js
@@ -0,0 +1,132 @@
+//Script JS - Site Ergonomission
+
+//LOGIN
+$('#login').on("click",()=>{
+	let email = $("#content-1 input[name='email']").val();
+	let senha = $("#content-1 input[name='senha']").val();
+	let url = "http://localhost:3000/api";
+
+	$.ajax({
+		type:'POST',
+		url: url,
+		data:{
+			"email":email,
+			"senha": senha
+		},
+		dataType: "json",
+		success: function(data){
+			let textinho = "Login feito com sucesso!";
+			$("#sucesso_login").append(textinho);
+
+			let uid = new URLSearchParams();
+			uid.append("uid",data.uid);
+			window.location.href="login.html?" + uid.toString();
+
+			//window.location.href = 'login.html';
+		}
+	});
+
+});
+
+//CADASTRO 
+
+$('#cadastrar').on("click", ()=>{
+	let nome = $("#content-2 input[name='nome']").val();
+	let email = $("#content-2 input[name='email']").val();
+	let confirme_email = $("#content-2 input[name='confirme_email']").val();
+	let senha = $("#content-2 input[name='senha']").val();
+	let confirme_senha = $("#content-2 input[name='confirme_senha']").val();
+	let url = "http://localhost:3000/api";
+
+	if (email != confirme_email){
+		alert("Email diferente!!");
+		return;
+	}
+	if(senha != confirme_senha){
+		alert("Senha diferente!!");
+		return;
+	}
+
+	$.ajax({
+		type:'PUT',
+		url: url,
+		data:{
+			"nome":nome,
+			"email":email,
+			"confirme_email":confirme_email,
+			"senha":senha,
+			"confirme_senha":confirme_senha
+		},
+		dataType:"json",
+		success: function(data){
+			let textinho2 = "Cadastro feito com sucesso!";
+			$("#sucesso_cadastro").append(textinho2);
+		}
+
+	});
+});
+
+//ATUALIZAR CADASTRO
+$('#atualizar').on("click", ()=>{
+	let email = $("#content-3 input[name='email']").val();
+	let novo_email = $("#content-3 input[name='novo_email']").val();
+	let senha = $("#content-3 input[name='senha']").val();
+	let nova_senha = $("#content-3 input[name='nova_senha']").val();
+	let confirme_nova_senha = $("#content-3 input[name='corfime_nova_senha']").val();
+	let url = "http://localhost:3000/api";
+	
+	$.ajax({
+		type:'PATCH',
+		url: url,
+		data:{
+			"email":email,
+			"novo_email":novo_email,
+			"senha":senha,
+			"nova_senha":nova_senha,
+			"confirme_nova_senha": confirme_nova_senha
+		},
+		success: function(data){
+			let textinho4 = "Usuário atulizado com sucesso!";
+			$('#sucesso_atualiza').append(textinho4);
+		}
+	})
+});
+
+
+//DELETAR USUÁRIO
+
+$('#deletar').on("click", ()=>{
+	let senha = $("#content-4 input[name='senha']").val();
+	let uid = new URLSearchParams(window.location.search).get("uid");
+	let url = "http://localhost:3000/api";
+	console.log(uid);
+	$.ajax({
+		type:'DELETE',
+		url: url,
+		data:{
+			"uid":uid,
+			"senha":senha
+		},
+		success: function(data){
+			let textinho3 = "Usuário deletado com sucesso!";
+			$('#sucesso_deletar').append(textinho3);
+		}
+	});
+});
+
+//PEDIR ALONGAMENTO
+$(document).ready(function(){
+	$('#alongamento').on("click", ()=>{
+		let des_alonga = $('#content-5 alongamentos').val();
+		let url = "http://localhost:3000/api/alongs/1";
+
+		$.ajax({
+			type:'GET',
+			url:url,
+			success: function(data){
+				$('#alonga_aqui').text(data);
+			}
+		});
+
+	});
+});
