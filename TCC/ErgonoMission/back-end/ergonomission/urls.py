aabd4947745f16209554e6ce31fb5daa634b23a9
Tiagocf <tiago.tcf2@gmail.com>
2021-12-20 21:29:47 -0300

    Add View de Autenticação

--- TCC/ErgonoMission/back-end/ergonomission/urls.py
+++ TCC/ErgonoMission/back-end/ergonomission/urls.py
@@ -1,13 +1,19 @@
 from django.contrib import admin
 from django.urls import path, include
 from rest_framework import routers
+from rest_framework.authtoken import views
 from core.views import * 
 
 router = routers.DefaultRouter()
 router.register(r'usuarios', UsuarioViewSet)
+router.register(r'pomodoros', PomodoroViewSet)
+router.register(r'alongamentos', AlongamentoViewSet)
+router.register(r'personagens', PersonagemViewSet)
+router.register(r'cosmeticos', CosmeticoViewSet)
 
 urlpatterns = [
     path('', include(router.urls)),
     path('api-auth/', include('rest_framework.urls', namespace='NRF')),
     path('admin/', admin.site.urls),
+    path('auth/', views.obtain_auth_token),
 ]
