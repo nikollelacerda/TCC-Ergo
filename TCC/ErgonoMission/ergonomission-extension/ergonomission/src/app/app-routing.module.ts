200ecaf15cbafcf67a316ee313ff5d0414d9a3f2
Nikolle de Lacerda Nascimento <nikolledelacerda@gmail.com>
2021-12-30 18:21:59 -0300

    EM01-mais alterações

--- TCC/ErgonoMission/ergonomission-extension/ergonomission/src/app/app-routing.module.ts
+++ TCC/ErgonoMission/ergonomission-extension/ergonomission/src/app/app-routing.module.ts
@@ -1,17 +1,7 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [
-  {
-    path:'',
-    pathMatch:'full',
-    redirectTo:'login',
-  },
-  {
-    path:'login',
-    //loadChildren:()=> import('./login/login.module').then((m)=>m.LoginModule),
-  }
-];
+const routes: Routes = [];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- TCC/ErgonoMission/ergonomission-extension/ergonomission/src/app/app.module.ts
+++ TCC/ErgonoMission/ergonomission-extension/ergonomission/src/app/app.module.ts
@@ -3,7 +3,7 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-//import { LoginModule } from './login/login.module';
+
 
 @NgModule({
   declarations: [
