19decd237391714b91d9760c05353ae394962f78
Tiago Civatti Frausino <33489675+Tiagocf2@users.noreply.github.com>
2021-12-29 23:04:22 -0300

    EM-02-Termino Usuario Controller

--- TCC/ErgonoMission/ergonomission-extension/ergonomission/src/controllers/usuarios.service.ts
+++ TCC/ErgonoMission/ergonomission-extension/ergonomission/src/controllers/usuarios.service.ts
@@ -7,13 +7,38 @@ import { Observable } from 'rxjs';
   providedIn: 'root'
 })
 export class UsuariosService {
-  httpHeaders = new HttpHeaders(BASE_HEADERS);
+  httpHeaders = {
+    headers: new HttpHeaders(BASE_HEADERS)
+  };
   constructor(private http: HttpClient) { }
 
   listUsuarios() : Observable<any> {
     return this.http.get(
       `${BASE_URL}usuarios/`, 
-      {headers: this.httpHeaders}
+      this.httpHeaders
     );
+  }
+
+  readUsuario(id : number) : Observable<any> {
+    return this.http.get(
+      `${BASE_URL}usuario/${id}/`,
+      this.httpHeaders
+    );
+  }
+
+  updateUsuario(data : any) : Observable<any> {
+    return this.http.put(
+      `${BASE_URL}usuario/${data.id}`,
+      data,
+      this.httpHeaders
+    );
+  }
+
+  createUsuario(data : any) : Observable<any> {
+     return this.http.post(
+      `${BASE_URL}usuario/`,
+      data,
+      this.httpHeaders
+     );
   }
 }
