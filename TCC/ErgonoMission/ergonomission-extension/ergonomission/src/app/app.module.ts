7891f7b4daa849e2ce9e001943fe3797ea8e1012
Nikolle de Lacerda Nascimento <nikolledelacerda@gmail.com>
2021-12-30 20:03:11 -0300

    EM01- atualização

--- TCC/ErgonoMission/ergonomission-extension/ergonomission/src/app/app.module.ts
+++ TCC/ErgonoMission/ergonomission-extension/ergonomission/src/app/app.module.ts
@@ -3,6 +3,7 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { LoginModule } from './login/login.module';
 
 
 @NgModule({
@@ -11,7 +12,8 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    LoginModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
