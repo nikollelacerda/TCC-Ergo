f509cdf07a955eee9f08336c821a31a38a9a5727
Nikolle de Lacerda Nascimento <nikolledelacerda@gmail.com>
2021-12-30 18:13:38 -0300

    EM01- refazendo umas coisas

--- TCC/ErgonoMission/ergonomission-extension/ergonomission/src/app/app-routing.module.ts
+++ TCC/ErgonoMission/ergonomission-extension/ergonomission/src/app/app-routing.module.ts
@@ -5,11 +5,11 @@ const routes: Routes = [
   {
     path:'',
     pathMatch:'full',
-    redirectTo:'home',
+    redirectTo:'login',
   },
   {
-    path:'home',
-    loadChildren:()=> import('./home/home.module').then((m)=>m.HomeModule),
+    path:'login',
+    //loadChildren:()=> import('./login/login.module').then((m)=>m.LoginModule),
   }
 ];
 
