b0e83a3947f25d99d8ab400c398693d2fea55c69
Tiago Civatti Frausino <33489675+Tiagocf2@users.noreply.github.com>
2021-12-31 18:30:16 -0300

    EM-12-Implementando nas viewsets

--- TCC/ErgonoMission/back-end/core/views.py
+++ TCC/ErgonoMission/back-end/core/views.py
@@ -1,11 +1,24 @@
 from django.shortcuts import render
 from rest_framework import viewsets
+from rest_framework.decorators import api_view, permission_classes
+from rest_framework.permissions import AllowAny, IsAuthenticated, IsAdminUser
 from .models import *
 from .serializers import *
 
 class UsuarioViewSet(viewsets.ModelViewSet):
     queryset = Usuario.objects.all()
     serializer_class = UsuarioSerializer
+
+    def get_permissions(self):
+        if self.action in ['update']:
+            permission_classes = [IsAuthenticated]
+        elif self.action in ['destroy', 'list']:
+            permission_classes = [IsAdminUser]
+        else:
+            permission_classes = [AllowAny]
+        return [permission() for permission in permission_classes]
+
+
 
 class PomodoroViewSet(viewsets.ModelViewSet):
     queryset = Pomodoro.objects.all()
@@ -15,6 +28,13 @@ class AlongamentoViewSet(viewsets.ModelViewSet):
     queryset = Alongamento.objects.all()
     serializer_class = AlongamentoSerializer
 
+    def get_permissions(self):
+        if self.action in ['destroy', 'create', 'update', 'partial_update']:
+            permission_classes = [IsAdminUser]
+        else:
+            permission_classes = [AllowAny]
+        return [permission() for permission in permission_classes]
+
 class PersonagemViewSet(viewsets.ModelViewSet):
     queryset = Personagem.objects.all()
     serializer_class = PersonagemSerializer
@@ -22,6 +42,13 @@ class PersonagemViewSet(viewsets.ModelViewSet):
 class CosmeticoViewSet(viewsets.ModelViewSet):
     queryset = Cosmetico.objects.all()
     serializer_class = CosmeticoSerializer
+
+    def get_permissions(self):
+        if self.action in ['destroy', 'create', 'update', 'partial_update']:
+            permission_classes = [IsAdminUser]
+        else:
+            permission_classes = [AllowAny]
+        return [permission() for permission in permission_classes]
 
 class HistoricoViewSet(viewsets.ModelViewSet):
     queryset = Historico.objects.all()
