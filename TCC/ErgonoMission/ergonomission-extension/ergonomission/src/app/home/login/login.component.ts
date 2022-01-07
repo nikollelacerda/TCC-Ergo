69c5bda6eff4130f248ff78ff52ccde9523fa16e
Nikolle de Lacerda Nascimento <nikolledelacerda@gmail.com>
2022-01-07 11:03:37 -0300

    validação de usuário

--- TCC/ErgonoMission/ergonomission-extension/ergonomission/src/app/home/login/login.component.ts
+++ TCC/ErgonoMission/ergonomission-extension/ergonomission/src/app/home/login/login.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Login } from 'src/app/models';
 
 @Component({
   selector: 'app-login',
@@ -6,10 +7,11 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./login.component.css']
 })
 export class LoginComponent implements OnInit {
-
+  model= new Login("","");
   constructor() { }
 
   ngOnInit(): void {
+    console.log(this.model);
   }
 
 }
