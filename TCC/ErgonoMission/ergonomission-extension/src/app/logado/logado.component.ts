ad07f78b9f84209ccb6594959a4953e22199dcc4
Tiago Civatti Frausino <33489675+Tiagocf2@users.noreply.github.com>
2022-01-13 01:19:52 -0300

    Implementação do Modulo Logado

---
+++ TCC/ErgonoMission/ergonomission-extension/src/app/logado/logado.component.ts
@@ -0,0 +1,53 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-logado',
+  templateUrl: './logado.component.html',
+  styleUrls: ['./logado.component.css']
+})
+export class LogadoComponent implements OnInit {
+
+  MenuOptions = [
+    {
+      label: 'Perfil de Usuário',
+      value: 'perfil',
+      img:'perfil.png'
+    },
+    {
+      label: 'Pomodoro',
+      value: 'pomodoro',
+      img:'play.png'
+    },
+    {
+      label: 'Biblioteca de Alongamentos',
+      value: 'biblioteca-alongamento',
+      img:'alongamento.png'
+    },
+    {
+      label: 'Loja',
+      value: 'loja',
+      img:'loja.png'
+    },
+    {
+      label: 'Informações Sobre Ergonomia',
+      value: 'ergonomia_info',
+      img:'trabalhando.png'
+    },
+    {
+      label: 'Sobre',
+      value: 'sobre',
+      img:'sobre.png'
+    },
+  ]
+  defaultOption = 'home';
+  currentOption = this.defaultOption;
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+  changeOption(option : string){
+    this.currentOption = option;
+  }
+}
--- TCC/ErgonoMission/ergonomission-extension/src/app/logado/logado.module.ts
+++ TCC/ErgonoMission/ergonomission-extension/src/app/logado/logado.module.ts
@@ -2,29 +2,31 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 import { LogadoRoutingModule } from './logado-routing.module';
-import { LogadoHomeComponent } from './logado-home/logado-home.component';
+import { HomeComponent } from './home/home.component';
 import { PerfilComponent } from './perfil/perfil.component';
 import { PomodoroComponent } from './pomodoro/pomodoro.component';
 import { BibliotecaAlogamentoComponent } from './biblioteca-alogamento/biblioteca-alogamento.component';
 import { LojaComponent } from './loja/loja.component';
 import { ErgonomiaInfoComponent } from './ergonomia-info/ergonomia-info.component';
 import { SobreComponent } from './sobre/sobre.component';
+import { LogadoComponent } from './logado.component';
 
 
 @NgModule({
   declarations: [
-    LogadoHomeComponent,
+    BibliotecaAlogamentoComponent,
+    ErgonomiaInfoComponent,
+    HomeComponent,
+    LojaComponent,
+    LogadoComponent,
     PerfilComponent,
     PomodoroComponent,
-    BibliotecaAlogamentoComponent,
-    LojaComponent,
-    ErgonomiaInfoComponent,
-    SobreComponent
+    SobreComponent,
   ],
   imports: [
     CommonModule,
     LogadoRoutingModule
   ],
-  exports:[LogadoHomeComponent]
+  exports:[LogadoComponent]
 })
 export class LogadoModule { }
