75b1e063fad40d080df215beee22bd7d9b00804d
Tiago Civatti Frausino <33489675+Tiagocf2@users.noreply.github.com>
2022-01-15 17:11:32 -0300

    EM-02-Componente: Biblioteca

--- TCC/ErgonoMission/ergonomission-extension/src/app/logado/biblioteca-alongamento/biblioteca-alongamento.component.ts
+++ TCC/ErgonoMission/ergonomission-extension/src/app/logado/biblioteca-alongamento/biblioteca-alongamento.component.ts
@@ -1,17 +1,39 @@
-import { Component, OnInit, Type } from '@angular/core';
+import { AfterViewInit, Component, OnInit } from '@angular/core';
+import PopupDefault from 'src/app/componentes/popup/default';
+import { PopupService } from 'src/app/componentes/popup/popup.service';
+import DefaultComponent from 'src/app/utils/default-component';
+import { AlongamentosService } from 'src/controllers/alongamentos.service';
 
 @Component({
   selector: 'app-biblioteca-alongamento',
   templateUrl: './biblioteca-alongamento.component.html',
   styleUrls: ['./biblioteca-alongamento.component.css']
 })
-export class BibliotecaAlongamentoComponent implements OnInit {
-
-  constructor() {
-   }
+export class BibliotecaAlongamentoComponent extends DefaultComponent implements OnInit {
+  ListaAlongamentos: AlongamentoItem[] = [];
   data: any;
 
-  ngOnInit(): void {
-  }
 
+  constructor(
+    private alongamentoService: AlongamentosService,
+    private popupService: PopupService
+  ) { super() }
+
+
+  ngOnInit(): void {
+    this.alongamentoService.listAlongamentos().subscribe(
+      data => {
+        this.ListaAlongamentos = data;
+        console.log(this.ListaAlongamentos)
+      },
+      error => {
+        this.popupService.open({ content: PopupDefault, data: { title: "Erro", message: error } })
+      }
+    )
+  }
+}
+
+interface AlongamentoItem {
+  imagem: string,
+  descricao: string,
 }
