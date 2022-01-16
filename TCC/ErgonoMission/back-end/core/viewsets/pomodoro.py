d45c05687b19ece49563b320c1e06da5e8f9acf6
Tiago Civatti Frausino <33489675+Tiagocf2@users.noreply.github.com>
2022-01-16 02:07:09 -0300

    EM-11-Pomodoro Update

--- TCC/ErgonoMission/back-end/core/viewsets/pomodoro.py
+++ TCC/ErgonoMission/back-end/core/viewsets/pomodoro.py
@@ -1,8 +1,11 @@
+import datetime
 from rest_framework import viewsets, status
 from rest_framework.response import Response
-from core.models import Pomodoro
+from rest_framework.permissions import AllowAny, IsAuthenticated, IsAdminUser
+
+from core.models import Historico, Pomodoro
 from core.serializers import PomodoroSerializer
-from ergonomission.helpers import calcular_pontos
+from ergonomission.helpers import MENSAGEM_POMODORO_CONCLUIDO, MENSAGEM_POMODORO_ENCERRADO, POMODORO_STATUS_CONCLUIDO, POMODORO_STATUS_ENCERRADO, POMODORO_STATUS_INATIVO, calcular_pontos, parse_mensagem
 
 
 
@@ -10,8 +13,17 @@ class PomodoroViewSet(viewsets.ModelViewSet):
     queryset = Pomodoro.objects.all()
     serializer_class = PomodoroSerializer
 
+    def get_permissions(self):
+        permission_classes = [AllowAny]
+        if self.action == ['destroy', 'update', 'partial_update']:
+            permission_classes = [IsAdminUser]
+        elif self.action == 'create':
+            permission_classes = [IsAuthenticated]
+
+        return [permission() for permission in permission_classes]
+
     def create(self, request):
-        request.data["usuario_uid"] = request.user.uid
+        request.data["usuario"] = request.user.uid
         serializer = PomodoroSerializer(data=request.data)
 
         if not serializer.is_valid():
@@ -26,9 +38,24 @@ class PomodoroViewSet(viewsets.ModelViewSet):
             pontos += pomodoro.usuario.pontos
             pomodoro.usuario.update(pontos=pontos)
 
+            if not (pomodoro.status == POMODORO_STATUS_INATIVO[0]):
+                if(pomodoro.status == POMODORO_STATUS_CONCLUIDO[0]):
+                    descricao = MENSAGEM_POMODORO_CONCLUIDO
+                if(pomodoro.status == POMODORO_STATUS_ENCERRADO[0]):
+                    descricao = MENSAGEM_POMODORO_ENCERRADO
+                descricao = parse_mensagem(descricao, pomodoro.titulo, pontos)
+                    
+                Historico.objects.create(
+                    usuario=pomodoro.usuario,
+                    descricao=descricao,
+                    data=datetime.datetime.now()
+                )
+
             return Response(
                 {
-                    "status": "Sucesso ao criar pomodoro"
+                    "status": "Sucesso ao criar pomodoro",
+                    "pontos": pontos,
+                    "mensagem": descricao
                 },
                 status=status.HTTP_201_CREATED
             )
@@ -37,3 +64,4 @@ class PomodoroViewSet(viewsets.ModelViewSet):
                 {"error":"Erro no banco de dados"},
                 status=status.HTTP_500_INTERNAL_SERVER_ERROR
             )
+    #END create#
