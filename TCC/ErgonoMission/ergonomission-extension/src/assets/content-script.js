fba438eef9dc1b9152f3dc26c80522ef79926d4d
Tiago Civatti Frausino <33489675+Tiagocf2@users.noreply.github.com>
2022-01-23 20:23:36 -0300

    Filtro sei la

--- TCC/ErgonoMission/ergonomission-extension/src/assets/content-script.js
+++ TCC/ErgonoMission/ergonomission-extension/src/assets/content-script.js
@@ -1,26 +1,35 @@
-import {STORAGE_FILTRO} from '../utils/constants';
-
+const STORAGE_FILTRO = 'filtroStatus';
 const FILTRO_CLASS = 'filtro';
 
-console.log('PINTO')
+console.log('Ergonomission está rodando nesta pagina!');
 
 chrome.storage.onChanged.addListener(
-    (changes, areaName) => { 
-        if(areaName === "sync" && STORAGE_FILTRO in changes){
-            handleFilter(changes[STORAGE_FILTRO]);
+    (changes, areaName) => {
+        console.log(changes, areaName)
+        if (areaName === "sync" && STORAGE_FILTRO in changes) {
+            console.log('matches')
+            handleFilter(changes[STORAGE_FILTRO].newValue);
         }
     }
 )
 
+chrome.storage.sync.get(STORAGE_FILTRO,
+    (itens) => {
+        itens[STORAGE_FILTRO] != undefined &&
+            handleFilter(itens[STORAGE_FILTRO])
+    }
+);
+
 const handleFilter = (status) => {
+    console.log('Filtro:', status);
     let classList = document.body.classList;
 
-    if(!status){
+    if (!status) {
         classList.remove(FILTRO_CLASS);
         return;
     }
 
-    if(classList.contains(FILTRO_CLASS)){
+    if (classList.contains(FILTRO_CLASS)) {
         classList.add(FILTRO_CLASS);
     }
 }
